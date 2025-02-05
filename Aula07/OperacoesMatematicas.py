b1ef3d8af1d6f755d7676254962f2677a860614e
Beto Rocha <roberto.blockchainresources@gmail.com>
2025-02-05 15:05:22 -0300

    oi

---
+++ Aula07/OperacoesMatematicas.py
@@ -0,0 +1,23 @@
+# + adição
+# - subtração
+# * multiplicação
+# / divisão
+# ** potência
+# // divisão inteira
+# % resto da divisão
+
+5 + 2 == 7
+5 - 2 == 3
+5 * 2 == 10
+5 / 2 == 2.5
+5 ** 2 == 25
+5 // 2 == 2
+5 % 2 == 1
+
+# Ordem de Precedência
+
+# 1. ()
+# 2. **
+# 3. *, /, //, %
+# 4. +, -
+
