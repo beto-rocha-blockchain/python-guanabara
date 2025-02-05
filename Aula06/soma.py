b41b449cacd032a9d488ea9dc4182c2a08d2e30f
Beto Rocha <roberto.blockchainresources@gmail.com>
2025-02-05 14:45:52 -0300

    oi

---
+++ Aula06/soma.py
@@ -0,0 +1,10 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número: '))
+s = n1 + n2
+# print('A soma vale', s)
+print('A soma vale{}'.format(s))
+
+# int = [7, -4, 0, 4875]
+# float = [4.5, 0.076, -15.223, 7.0]
+# bool = [True, False]
+# str = ['Olá', '7.5', '']
