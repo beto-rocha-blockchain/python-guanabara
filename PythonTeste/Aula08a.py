2719ad49f4d5628e3e98961d999b7d6543868813
Beto Rocha <roberto.blockchainresources@gmail.com>
2025-02-25 20:57:36 -0300

    oi

---
+++ PythonTeste/Aula08a.py
@@ -0,0 +1,7 @@
+#from math import sqrt, floor
+#num = int(input('Digite um número: '))
+#raiz = sqrt(num)
+#print('A raiz de {} e igual a {}'.format(num, floor(raiz)))
+
+import emoji
+print(emoji.emojize("Olá, 
--- project/PythonExercicios/ex008.py
+++ project/PythonExercicios/ex008.py
@@ -1,9 +1,18 @@
+medida = float(input('Uma distância em metros: '))
+cm= medida * 100
+mm= medida * 1000
+print('A medida de {}m corresponde a {:.0f}cm e {:.0f}mm`.format(medida, cm, mm) ')
+
+
+
+
+
 # Solicita ao usuário um valor em metros
-metros = float(input("Digite um valor em metros: "))
+#metros = float(input("Digite um valor em metros: "))
 
 # Converte para centímetros e milímetros
-centimetros = metros * 100
-milimetros = metros * 1000
+#centimetros = metros * 100
+#milimetros = metros * 1000
 
 # Exibe os resultados
-print(f"{metros} metros equivalem a {centimetros} centímetros e {milimetros} milímetros.")
+#print(f"{metros} metros equivalem a {centimetros} centímetros e {milimetros} milímetros.")
